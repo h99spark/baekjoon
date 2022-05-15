94333e34777cb82138099bb3fc40a0c56104c19c
h99spark <92707259+h99spark@users.noreply.github.com>
2022-05-16 07:18:33 +0900

    Update baekjoon_15552.py
    rstrip 추가

--- baekjoon_15552.py
+++ baekjoon_15552.py
@@ -1,6 +1,6 @@
 import sys
 
-n = int(sys.stdin.readline())
+n = int(sys.stdin.readline().rstrip())
 for _ in range(n):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, sys.stdin.readline().rstrip().split())
     print(a+b)
