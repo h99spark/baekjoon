7aa4c780ee0a69a199dbca1cc586e10ba0a86646
h99spark <92707259+h99spark@users.noreply.github.com>
2022-05-16 22:09:45 +0900

    Update baekjoon_10989.py

--- baekjoon_10989.py
+++ baekjoon_10989.py
@@ -1,12 +1,11 @@
-import sys
+n = int(input())
+numbers = [int(input()) for _ in range(n)]
 
-n = int(sys.stdin.readline())
-lists = [0] * 10001
+cnt_list = [0] * (n+1)
+for num in numbers:
+    cnt_list[num] += 1
 
-for i in range(n):
-    lists[int(sys.stdin.readline())] += 1
-
-for i in range(10001):
-    if lists[i] != 0:
-        for j in range(lists[i]):
+for i in range(1, n+1):
+    if cnt_list[i] != 0:
+        for j in range(cnt_list[i]):
             print(i)
