7aec033c95913e4fd53427e922e570e11bcdf232
h99spark <92707259+h99spark@users.noreply.github.com>
2022-05-17 00:56:17 +0900

    Add files via upload

---
+++ baekjoon_10814.py
@@ -0,0 +1,11 @@
+n = int(input())
+members = []
+for _ in range(n):
+    age, name = input().split()
+    age = int(age)
+    members.append((age, name))
+
+members.sort(key = lambda x: x[0])
+
+for member in members:
+    print(member[0], member[1])
